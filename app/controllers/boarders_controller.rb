3f0c2a669a5ef3237c0ec2fc73b84b21aa8e41d3
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-02 09:38:41 -0700

    Add boarders controller and routes

---
+++ app/controllers/boarders_controller.rb
@@ -0,0 +1,35 @@
+class BoardersController < ApplicationController
+  def index
+    @boarders = Boarder.all
+    json_response(@boarders)
+  end
+
+  def show
+    @boarder = Boarder.find(params[:id])
+    json_response(@boarder)
+  end
+
+  def create
+    @boarder = Boarder.create(boarder_params)
+    json_response(@boarder)
+  end
+
+  def update
+    @boarder = Boarder.find(params[:id])
+    @boarder.update(boarder_params)
+  end
+
+  def destroy
+    @boarder = Boarder.find(params[:id])
+    @boarder.destroy
+  end
+
+  private
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
+
+  def boarder_params
+    params.permit(:name, :kind, :breed, :gender, :age, :description)
+  end
+end
