ee125c535b42e284c43536eab669b37b345601d1
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-04-03 17:14:02 -0700

    Add post_boarder spec

--- spec/requests/get_boarders_spec.rb
+++ spec/requests/get_boarders_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe "get all boarders route", type: :request do
   before do
     @branch = FactoryBot.create(:branch_with_boarders)
-    branch_id = @branch.id
-    get api_v1_branch_boarders_path(branch_id)
+    get api_v1_branch_boarders_path(@branch.id)
   end
   
   it 'returns all boarders at a branch' do
---
+++ spec/requests/post_boarder_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "post a boarder route", type: :request do
+  before do
+    @branch = Branch.create(location: 'Portland')
+    post api_v1_branch_boarders_path(@branch.id), params: {branch_id: "#{@branch.id}",
+                                                           name: 'Fluffy',
+                                                           kind: 'Cat',
+                                                           breed: 'Manx'}
+  end
+
+  it 'creates a boarder' do
+    expect(JSON.parse(response.body)['name']).to eq('Fluffy')
+  end
+
+  it 'returns status code 201' do
+    expect(response).to have_http_status(:created)
+  end
+end
